1ae05e5c703f7238528ac598c337c411b83f815a
Dan habib <57772926+Snowy-27@users.noreply.github.com>
2020-08-24 13:44:24 +0200

    Update googleWhatsapp.py

--- googleWhatsapp.py
+++ googleWhatsapp.py
@@ -11,11 +11,11 @@ def index():
     else:
         message = request.args.get('message')
 
-    client = Client('AC57e474a2839a87536a6c91ce495e5fa1', '16f9168155d0e2bbf463feb7c749e5b0')
+    client = Client('', '')
     whMessage = client.messages.create(
         from_='whatsapp:+14155238886',
         body=message,
-        to='whatsapp:+33651598405')
+        to='whatsapp:+')
     return "Message envoyé"
 
 
